625d902ae4203e57a18545809819d850e61e7a6a
james-burgess <jms.bgr@gmail.com>
2017-10-26 17:17:41 +0200

    added some thaughts and ideas

--- arrayChecker/array_creator.py
+++ arrayChecker/array_creator.py
@@ -1,4 +1,9 @@
-from array_viability_test import viable
+#Example Co-ords
+
+import numpy as np
+print( np.linspace(-6000, 6000, 60)[1])
+
+# y = np.random.randn(60)*100
 
 #create stepper function
 def inc(value, step): return value + step
@@ -9,15 +14,24 @@ def step_range(start, end, step, func):
 
 #create array of cordinates
 co_ords = [(x,y) for x in step_range(12, 20000, 12, inc) for y in step_range(12, 20000, 12, inc)]
+
+#smaller array to save processing time
+# co_ords = [[x,y] for x in step_range(12, 200, 12, inc) for y in step_range(12, 200, 12, inc)]
+
+
 print(len(co_ords))
+#>256
 
-#Example Co-ords
-# x = np.linspace(-6000, 6000, 60)
-# y = np.random.randn(60)*100
+# for one node we would have 256 possible placements
+# therefore two nodes = 256**2 
+# but we still can optomise by factoring in the fact that they cant be diagonailly further than 20km. 
+#(unless one was in the center?)
 
-# for i in len(co_ords):
-#     new_c = co_ords[i]
+#so lets start with two nodes and map all the positions
 
-for single in co_ords:
-    if not(viable(*single)):
-        co_ords.pop(single)
+#let n1 be fixed at point 0,0 for now
+
+# possiblities = []
+
+# for x,i in co_ords:
+# 	possibitiles.append([])
